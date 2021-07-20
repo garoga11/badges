7bb02baa96ffad67bb91d1991fe44eaf6dfb6241
unknown <a6519150007@utch.edu.mx>
2021-07-20 17:58:19 -0600

    creating the app navigator

--- src/components/badgesScreen/BadgesTabNavigator.js
+++ src/components/badgesScreen/BadgesTabNavigator.js
@@ -3,8 +3,9 @@ import {Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
 import BadgesStack from './BadgesStack';
-import Colors from '../res/Colors';
+import Colors from '../../res/Colors';
 import FavoriteStack from '../Favorites/FavoritesStack';
+import UserStack from '../UsersScreen/UserStack';
 
 const Tabs = createMaterialTopTabNavigator();
 
@@ -23,27 +24,15 @@ const BadgesTabNavigator = () => {
       <Tabs.Screen
         name="Badges"
         component={BadgesStack}
-        options={{
-          tabBarIcon: ({size, color}) => (
-            <Image
-              style={{tintColor: color, width: size, height: size}}
-              source={require('../../assets/home.png')}
-            />
-          ),
-        }}
       />
 
       <Tabs.Screen
         name="Favorites"
         component={FavoriteStack}
-        options={{
-          tabBarIcon: ({size, color}) => (
-            <Image
-              style={{tintColor: color, width: size, height: size}}
-              source={require('../../assets/home.png')}
-            />
-          ),
-        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={UserStack}
       />
 
     </Tabs.Navigator>
