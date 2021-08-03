6e1ecd7391b65805217bcd53f4d9512b22aab5fe
unknown <a6519150007@utch.edu.mx>
2021-08-02 21:30:52 -0600

    comment events

--- src/components/badgesScreen/BadgesScreen.js
+++ src/components/badgesScreen/BadgesScreen.js
@@ -20,52 +20,67 @@ class BadgesScreen extends React.Component {
     badges: undefined,
     badgesCopy: undefined,
   };
-
+//next event calls to get the data (fetchdata) and also calls to the intervals
   componentDidMount() {
     this.fetchdata();
     this.focusEvent();
     this.blurEvent();
   }
 
+  //next event clear the interval that was set before
   focusEvent = () => {
     this.focusListener = this.props.navigation.addListener('focus', () => {
       this.setFetchInterval();
     });
   };
 
+//next event clear the interval that was set before
   blurEvent = () => {
     this.blurListener = this.props.navigation.addListener('blur', () => {
       clearInterval(this.interval);
     });
   };
 
+  // setting an interval of 3s
   setFetchInterval = () => {
     this.interval = setInterval(this.fetchdata, 3000);
   };
 
+  //next event get all the badges
   fetchdata = async () => {
     this.setState({loading: true});
     let response = await Http.instance.get_all();
     this.setState({loading: false, badges: response, badgesCopy: response});
   };
 
+  //next event shows the user details
   handlePress = item => {
+    //redirect the user to the badges details screen
     this.props.navigation.navigate('BadgesDetail', {item});
   };
 
+  //next event is to edit a badge
+
   handleEdit = item => {
+    //calls to BadgesEdit file
     this.props.navigation.navigate('BadgesEdit', {item});
   };
 
+//handleChange change the badges
+
   handleChange = query => {
+    //save the badges before changing them
     const {badgesCopy} = this.state;
 
+//get the badges with the filter
     const badgesFiltered = badgesCopy.filter(badge => {
       return badge.name.toLowerCase().includes(query.toLowerCase());
     });
 
+    //save the badges in the state
     this.setState({badges: badgesFiltered});
 
+    //clean the interval
     if (query) {
       clearInterval(this.interval);
     } else {
@@ -73,7 +88,10 @@ class BadgesScreen extends React.Component {
     }
   };
 
+//The next event deletes a badge
+
   handleDelete = item => {
+    //we send a alert to say to the user if he is sure of deleting a badge
     Alert.alert(
       'Are you sure?',
       `Do you really want to delete ${item.name}'s badge?\n\nThis process canot be undone.`,
@@ -99,6 +117,8 @@ class BadgesScreen extends React.Component {
       },
     );
   };
+
+//this event calls to focusListener and to blurListener
 
   componentWillUnmount() {
     this.focusListener();
